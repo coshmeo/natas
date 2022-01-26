b064cbf8667841cb0df09eddb40df774f250b4b0
jesse mailhot <coshmeo@gmail.com>
2022-01-26 15:10:22 -0800

    added more notes & a payload outline

---
+++ 20/payload.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'Authorization': 'Basic bmF0YXMyMDplb2ZtM1dzc2h4YzVid3RWbkV1R0lscjdpdmI5S0FCRg=='}
+
+url = 'http://natas20.natas.labs.overthewire.org/index.php'
+
