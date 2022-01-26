661efdcc03cc1ca2de657898773aad4880f256f5
jesse mailhot <coshmeo@gmail.com>
2022-01-26 14:11:47 -0800

    chagned the .format to one variable (was 2).

--- 19/recon.py
+++ 19/recon.py
@@ -7,6 +7,6 @@ headers = {'Authorization': 'Basic bmF0YXMxOTo0SXdJcmVrY3VabEE5T3NqT2tvVXR3VTZsa
 tests = ['a', 'aa', 'aaa', 'b', 'bb', 'bbb', 'ab', 'ba'] # some characters to test
 
 for test in tests:
-    url = 'http://natas19.natas.labs.overthewire.org/index.php?username={}&password={}'.format(test, test)
+    url = 'http://natas19.natas.labs.overthewire.org/index.php?username={}&password='.format(test)
     r = requests.post(url, headers=headers)
     print(test+'\t--> '+bytes.fromhex(r.cookies['PHPSESSID']).decode('ascii'))
