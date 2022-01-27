bc461e642d99cf7312a26ea3bed3960acacd5d36
jesse mailhot <coshmeo@gmail.com>
2022-01-27 15:31:57 -0800

    did some more recon, still no solution.

--- 20/recon.py
+++ 20/recon.py
@@ -3,9 +3,9 @@ from bs4 import BeautifulSoup
 
 headers = {'Authorization': 'Basic bmF0YXMyMDplb2ZtM1dzc2h4YzVid3RWbkV1R0lscjdpdmI5S0FCRg=='}
 
-testnames = ['admin','admin','admin','admin']
+testnames = ['a','ad','adm','admi','admin','admin','admin','admin','admin']
 
 for name in testnames:
     url = 'http://natas20.natas.labs.overthewire.org/index.php?name={}'.format(name)
     r = requests.post(url, headers=headers)
-    print(name+'\t--> '+r.cookies['PHPSESSID'])
+    print(name+'\t--> '+r.cookies['PHPSESSID']+'\tlen: '+str(len(r.cookies['PHPSESSID'])))
